1f7617690726502ae380aa174b3fa84c22738542
Artemiy Solopov <art-solopov@yandex.ru>
2017-06-19 20:26:04 +0300

    Lib & tests

--- lib/funbox_nmax.rb
+++ lib/funbox_nmax.rb
@@ -1,5 +1,24 @@
+require 'algorithms'
+require 'gmp'
+
 require "funbox_nmax/version"
 
 module FunboxNmax
-  # Your code goes here...
+  module_function
+
+  def n_max(n, io = $stdin)
+    heap = Containers::MinHeap.new
+
+    # TODO: consider using some other iteration method?
+    io.each_line(' ', 2000) do |line|
+      line.scan(/\d+/).each do |e|
+        heap << GMP::Z.new(e)
+        heap.pop if heap.length > n
+      end
+    end
+
+    result = []
+    result << heap.pop while heap.size.nonzero?
+    result.reverse
+  end
 end
--- spec/funbox_nmax_spec.rb
+++ spec/funbox_nmax_spec.rb
@@ -5,7 +5,18 @@ RSpec.describe FunboxNmax do
     expect(FunboxNmax::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'returns the maximum n numbers' do
+    let(:str) { '10 20 30 40 50 100 120 140' }
+    subject { FunboxNmax.n_max(3, StringIO.new(str)) }
+
+    it { expect(subject).to eq([140, 120, 100]) }
+
+    describe 'long numbers' do
+      let(:str) do
+        '1' + '0' * 999 + ' ' + '5' + '0' * 300 + ' ' + '9' * 120
+      end
+
+      it { expect(subject).to eq([10**999, 5 * 10**300, (10**120 - 1)]) }
+    end
   end
 end
